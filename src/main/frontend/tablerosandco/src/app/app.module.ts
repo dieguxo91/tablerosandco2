3b022d5ea77dfa81d7eca6e5daa041a68298fe8f
dieguxo91 <diegogarcia0512@gmail.com>
2023-05-07 14:32:48 +0200

    header e intento de bootstrap

--- src/main/frontend/tablerosandco/src/app/app.module.ts
+++ src/main/frontend/tablerosandco/src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppComponent } from './app.component';
 import { TableroComponent } from './tablero/tablero/tablero.component';
 import {BarajaModule} from "./baraja/baraja.module";
 import {TableroModule} from "./tablero/tablero.module";
-
+import { HeaderComponent } from './body/header/header.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BarajaModule,
-    TableroModule
+    TableroModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
